1ac6923272f0ee00e9b0b7c9f163ac0319807917
zhangpeng <pzhang@aibee.com>
2018-04-18 13:49:32 +0800

    MOD: docker run script cmd mod

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,3 @@
 # build image & dockerfile build
 ./mvnw install dockerfile:build
-docker run -e "SPRING_PROFILES_ACTIVE=production" -p 8080:8080 -t zpng/demo2:latest
+docker run -e "SPRING_PROFILES_ACTIVE=prod" -p 8080:8080 -t zpng/demo2:latest

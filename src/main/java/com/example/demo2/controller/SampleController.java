5fc2bc017c0ed1ff2296b4f8a4aac37531e99df5
zhangpeng <pzhang@aibee.com>
2018-04-18 12:03:18 +0800

    MOD: logback config change

--- src/main/java/com/example/demo2/controller/SampleController.java
+++ src/main/java/com/example/demo2/controller/SampleController.java
@@ -1,5 +1,7 @@
 package com.example.demo2.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
@@ -8,9 +10,17 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class SampleController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
+        logger.trace("日志输出 trace");
+        logger.debug("日志输出 debug");
+        logger.info("日志输出 info");
+        logger.warn("日志输出 warn");
+        logger.error("日志输出 error");
         return "Hello World!";
     }
 
